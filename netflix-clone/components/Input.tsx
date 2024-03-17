6157cd160b9849e455d72d4817bbf5a8b2bbaf39
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-17 14:25:42 +0530

    Added Styling to input Box and AAlso added inputProps

--- netflix-clone/components/Input.tsx
+++ netflix-clone/components/Input.tsx
@@ -1,9 +1,58 @@
 import React from 'react'
 
+
+// create Input Type props
+interface InputProps{
+  id:string;
+  onChange :any;
+  value:string;
+  label:string;
+  type?:string
+}
+
+
+
  const Input = () => {
   return (
-    <div>
-        <input type="text" />
+    <div className='relative'>
+        <input
+        id='email'
+         type="text"
+        className='
+        block
+        rounded-md
+        px-6
+        pt-6
+        w-full
+        text-md
+        text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer' 
+        placeholder=' '
+        />
+        <label htmlFor="email"
+        className='
+        absolute
+        text-md
+        text-zinc-400
+        duration-150
+        transform
+        -translate-y-3
+        scale-75
+        top-4
+        z-10
+        origin-[0]
+        left-6
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+        '
+        
+        >Email</label>
     </div>
   )
 }
